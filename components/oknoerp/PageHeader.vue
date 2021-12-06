779f7a4fc3465d6aad85868e0d6685d6ef8a182d
Roman Gnatyshen <r.gnatishen@axioma.pl>
2021-12-06 20:45:25 +0100

    css correction

--- components/oknoerp/PageHeader.vue
+++ components/oknoerp/PageHeader.vue
@@ -28,6 +28,63 @@
     }
 </script>
 
-<style scoped>
- 
+<style lang="scss" scoped>
+
+ .oknoerp-trusted {
+  background-image: url("/images/breadcrumb-img.jpg");
+}
+
+.oknoerp-breadcrumb-area {
+  padding: 280px 0 140px 0;
+  @media only screen and (min-width: 480px) and (max-width: 767px) {
+    padding: 240px 0 140px 0;
+  }
+  @media only screen and (min-width: 320px) and (max-width: 479px) {
+    padding: 240px 0 140px 0;
+  }
+
+  .trusted-content {
+    .trusted__title {
+      .trusted__title-title {
+        font-size: 70px;
+        @media only screen and (min-width: 480px) and (max-width: 767px) {
+          font-size: 60px;
+        }
+        @media only screen and (min-width: 320px) and (max-width: 479px) {
+          font-size: 50px;
+        }
+        @media (max-width: 320px) {
+          font-size: 42px;
+        }
+      }
+      .bread__rumb-item {
+        margin-bottom: 5px;
+        li {
+          display: inline-block;
+          font-size: 16px;
+          font-weight: 600;
+          position: relative;
+          padding-right: 22px;
+          a {
+            color: #fff;
+          }
+          &.active-item {
+            &:after {
+              position: absolute;
+              content: "-";
+              top: 0;
+              right: 0;
+              padding: 0 7px;
+              font-size: 16px;
+            }
+          }
+          &:last-child {
+            padding-right: 0;
+          }
+        }
+      }
+    }
+  }
+}
+
 </style>
